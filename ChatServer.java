57c336564f8abb890fa4a14c98b6995763e52c03
akiyamayummy <842720660@qq.com>
2018-01-17 08:17:54 +0800

    init

---
+++ ChatServer.java
@@ -0,0 +1,279 @@
+package websocket.chat;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint(value = "/websocket/chat")
+public class ChatServer {
+	static PrintStream debug = System.out;
+	
+    private static final Set<ChatServer> connections =
+            new CopyOnWriteArraySet<ChatServer>();
+    private static final Map<Integer,Object[]> groups = 
+    		new TreeMap<Integer,Object[]>();
+    private static final int MAX_G = 100;
+    private static final int MAX_U = 1000;
+    private static Boolean[] UID_SET = null,GID_SET = null;
+    private static final String[] CMDS = {
+    		"USER","NEWG","JOIN","LINK","EXIT","TEXT"
+    };
+    private static final String[] COPS = {
+    		"USER [^ ]+",
+    		"NEWG [^ ]+",
+    		"JOIN [0-9]+",
+    		"LINK [0-9]+ [^ ]+",
+    		"EXIT [0-9]+",
+    		"TEXT [0-9]+ [^ ]+"
+    };
+    private static final boolean isCmd(String str){
+	    for(int i=0;i<CMDS.length;i++){
+	    	Pattern pattern = Pattern.compile(COPS[i]);
+	    	Matcher matcher = pattern.matcher(str);
+	    	if(matcher.matches())return true;
+	    }
+	    return false;
+    }
+    private int newUID(){
+    	if(UID_SET == null){
+    		UID_SET = new Boolean[MAX_U];
+    		for(int i=0;i<MAX_U;i++)UID_SET[i] = false;
+    	}
+    	for(int i=0;i<MAX_U;i++)if(!UID_SET[i]){
+    		UID_SET[i] = true;
+    		return i;
+    	}
+    	return -1;
+    }
+    private int newGID(){
+    	if(GID_SET == null){
+    		GID_SET = new Boolean[MAX_G];
+    		for(int i=0;i<MAX_G;i++)GID_SET[i] = false;
+    	}
+    	for(int i=0;i<MAX_G;i++)if(!GID_SET[i]){
+    		GID_SET[i] = true;
+    		return i;
+    	}
+    	return -1;
+    }
+    
+    private String nickname;
+    private Session session;
+    private int ID = -1;
+    private Set<Integer> joinedGroups = new TreeSet<Integer>();
+
+    @OnOpen
+    public void OnOpen(Session session) {
+    	debug.println("Open");
+    	this.session = session;  
+        connections.add(this); 
+    }
+
+    @OnClose
+    public void OnClose() {
+    	debug.println("Close");
+    	connections.remove(this);
+    	doBeforeClose();
+    	sendList();
+    }
+
+    @OnMessage
+    public void OnMessage(String message) {
+    	debug.println("Message : "+message);
+    	if(!isCmd(message)){
+    		debug.println("ban : "+message);
+    		return;
+    	}
+    	if(message.startsWith("USER")){
+			ID = newUID();
+			if(ID == -1)send("UFULL");
+			nickname = message.substring(5);
+			sendList();
+		}else if(message.startsWith("NEWG")){
+			int gid = newGID();
+			if(gid == -1)send("GFULL");
+			String gname = message.substring(5);
+			Object[] os = new Object[2];
+			os[0] = gname; os[1] = 1;
+			groups.put(gid,os);
+			joinedGroups.add(gid);
+			send("PUSH "+gid+" "+gname);
+			sendList();
+		}else if(message.startsWith("JOIN")){
+			int gid = Integer.valueOf(message.substring(5));
+			if(groups.containsKey(gid) && !joinedGroups.contains(gid)){
+				joinedGroups.add(gid);
+				Object[] os = groups.get(gid);
+				send("PUSH "+gid+" "+os[0]);
+				os[1] = (Integer)os[1]+1;
+				broadcast("TEXT "+gid+" <messagesys>"+nickname+"_加入了会话</messagesys>");
+			}
+			sendList();
+		}else if(message.startsWith("LINK")){
+			String[] cmds = message.split(" ");
+			int pid = Integer.valueOf(cmds[1]);
+			if(pid == ID)return;
+			ChatServer ps = null;
+			for(ChatServer client : connections){
+	    		if(client.ID == pid){
+	    			ps = client;
+	    			break;
+	    		}
+	    	}
+			if(ps == null)return;
+			int gid = newGID();
+			ps.joinedGroups.add(gid);
+			joinedGroups.add(gid);
+			ps.send("PUSH "+gid+" <linkwith_id=\"lw"+ID+"\"></linkwith>与_"+nickname+"_聊天中");
+			send("PUSH "+gid+" <linkwith_id=\"lw"+pid+"\"></linkwith>与_"+cmds[2]+"_聊天中");
+			sendList();
+		}else if(message.startsWith("EXIT")){
+			String[] ops = message.split(" ");
+			int gid = Integer.valueOf(ops[1]);
+			joinedGroups.remove(gid);
+			String linkLeaveMessage = "";
+			if(groups.containsKey(gid)){
+	    		Object[] os = groups.get(gid);
+	    		os[1] = (Integer)os[1]-1;
+	    		if((Integer)os[1] == 0)groups.remove(gid);
+    		}else linkLeaveMessage = "<leave_id=\"llv"+gid+"\"></leave>";
+			sendList();
+			broadcast("TEXT "+gid+" "+linkLeaveMessage+"<messagesys>"+nickname+"_已经离开了会话</messagesys>");
+		}else if(message.startsWith("TEXT")){
+			broadcast(message);
+		}
+    }
+
+    @OnError
+    public void onError(Throwable t) throws Throwable {
+    	t.printStackTrace();
+    }
+    
+    private static void sendList(){
+    	broadcast("_");
+    	for (ChatServer client : connections) {
+            try {
+                synchronized (client) {
+                    client.session.getBasicRemote().sendText(
+                    		"LIST "+
+                    		"{\"yourID\":"+client.ID+
+                    		",\"grouplist\":"+getGroupStr()+
+                    		",\"userlist\":"+getUserStr()+"}"
+                    );
+                }
+            } catch (IOException e) {
+                client.doBeforeClose();
+                connections.remove(client);
+                try {
+                    client.session.close();
+                } catch (IOException e1) {
+                    // Ignore
+                }
+            }
+        }
+    }
+    
+    static private String getGroupStr(){
+    	int con = 0;
+    	Object[][] os = new Object[MAX_G][3];
+    	for(Integer i : groups.keySet())os[con++][0] = i;
+    	con = 0;
+    	for(Object[] p : groups.values()){
+    		os[con][1] = p[0]; os[con][2] = p[1];
+    		con++;
+    	}
+    	String res = "[";
+    	for(int i=0;i<con;i++){
+    		//"groupname":"艹你妈","numberofpeople":3,"groupID":1
+			res += "{";
+    		res += "\"groupname\":\""+os[i][1]+"\","+
+    			   "\"numberofpeople\":"+os[i][2]+","+
+    			   "\"groupID\":"+os[i][0];
+    		res += "}"+(i==con-1?"":",");
+    	}
+    	res += "]";
+    	return res;
+    }
+    static private String getUserStr(){
+    	String res = "["; boolean has = false;
+    	for(ChatServer client : connections){
+    		//userID:10,nickname:"张三"
+    		if(has)res += ",";else has = true;
+    		res += "{";
+    		res += "\"userID\":"+client.ID+
+    				",\"nickname\":\""+client.nickname+"\"";
+    		res += "}";
+    	}
+    	res += "]";
+    	return res;
+    }
+
+    
+    public void doBeforeClose(){
+    	UID_SET[ID] = false; ID = -1;
+    	debug.println("joined : "+joinedGroups.size());
+    	for(int gid : joinedGroups){
+    		String linkLeaveMessage = "";
+    		if(groups.containsKey(gid)){
+	    		Object[] os = groups.get(gid);
+	    		os[1] = (Integer)os[1]-1;
+	    		if((Integer)os[1] == 0)groups.remove(gid);
+    		}else linkLeaveMessage = "<leave_id=\"llv+"+gid+"\"></leave>";
+    		broadcast("TEXT "+gid+" "+linkLeaveMessage+"<messagesys>"+nickname+"_已经离开了会话</messagesys>");
+    	}
+    }
+    
+    public boolean send(String msg){
+    	try {
+    		synchronized (this) {
+    			this.session.getBasicRemote().sendText(msg);
+    		}
+    		return true;
+		} catch (IOException e) {
+			doBeforeClose();
+			connections.remove(this);
+			try {
+                session.close();
+            } catch (IOException e1) {
+                // Ignore
+            }
+		}
+    	return false;
+    }
+    
+    private static void broadcast(String msg) {
+    	debug.println("broadcast : "+msg);
+        for (ChatServer client : connections) {
+            try {
+                synchronized (client) {
+                	if(client.session.isOpen())
+                		client.session.getBasicRemote().sendText(msg);
+                	else throw new IOException();
+                }
+            } catch (IOException e) {
+            	debug.println("err1 : "+e.getMessage());
+                client.doBeforeClose();
+                connections.remove(client);
+                try {
+                    client.session.close();
+                } catch (IOException e1) {
+                	debug.println("err2 : "+e1.getMessage());
+                    // Ignore
+                }
+            }
+        }
+    }
+}
